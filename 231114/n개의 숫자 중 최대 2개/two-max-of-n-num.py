979f93f468665c3404f45213733ab2bab748d334
KoYeongSoung <61423004+koto02003@users.noreply.github.com>
2023-11-14 23:24:06 +0900

    Title: n개의 숫자 중 최대 2개, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231114/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 231114/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -1,19 +1,21 @@
 n = int(input())
 arr = list(map(int, input().split()))
 maxNum = arr[0]
-maxIndex = -1
-maxIndex2 = -1
+maxIndex = 0
+
 for i in range(1, n):
     if maxNum < arr[i]:
         maxIndex = i
         maxNum = arr[i]
+        print(maxIndex)
 
+print(arr[maxIndex], end= ' ')
 maxNum = arr[0]
 for i in range(1, n):
     if i == maxIndex:
         continue
     if maxNum < arr[i]:
-        maxIndex2 = i
+        maxIndex = i
         maxNum = arr[i]
 
-print(arr[maxIndex], arr[maxIndex2])
+print(arr[maxIndex])
